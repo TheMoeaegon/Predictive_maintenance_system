454964b3fefcdaca5824453eec397bfd4f0712a0
Moee1149 <shahi.kshitij55@gmail.com>
2025-11-29 18:12:53 +0545

    added model training file

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,8 +1,8 @@
 class Config:
     # DATASET_PATH
-    DATASET_PATH = "/media/kshitij/external-drive/home/kshitij/workspace/github.com/moee1149/Predictive_maintenance_system/backend/train_val_test_split_20250924_035736.pkl"
-    MODEL_PATH = "/media/kshitij/external-drive/home/kshitij/workspace/github.com/moee1149/Predictive_maintenance_system/backend/trained_model_20250926_023409.keras"  # Update this too
-    COMPONENTS_PATH = "/media/kshitij/external-drive/home/kshitij/workspace/github.com/moee1149/Predictive_maintenance_system/backend/model_components_20250926_023409.pkl"  # Update this too
+    DATASET_PATH = "/home/kshitij/workspace/github.com/Predictive_maintenance_system/backend/train_val_test_split_20250924_035736.pkl"
+    MODEL_PATH = "/home/kshitij/workspace/github.com/Predictive_maintenance_system/backend/trained_model_20250926_023409.keras"  # Update this too
+    COMPONENTS_PATH = "/home/kshitij/workspace/github.com/Predictive_maintenance_system/backend/model_components_20250926_023409.pkl"  # Update this too
 
     PREDICTION_INTERVAL = 3
     MAX_HISTORY = 200
